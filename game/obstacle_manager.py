7ba34dfd52b80bd8a288a9dba6a27e8a9787fa6a
PipeDev\Lenovo <andres.giraldo47424@ucaldas.edu.co>
2025-09-26 19:06:08 -0500

    eliminacion archivos innecesarios

--- game/obstacle_manager.py
+++
@@ -1,8 +0,0 @@
-class Obstacle:
-    def __init__(self, x, y, width, height, type, damage):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.type = type
-        self.damage = damage
